ff69d3c8e9c269f9b3ff4002f6fa4a7fda6409d4
unknown <AmalMissaoui>
2020-04-14 21:55:15 +0100

    my commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+/*import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+(window as any).html2canvas = html2canvas;
+import htmlToImage from 'html-to-image';
+import { faRoad,faMobile,faEnvelope, faBook, faGamepad, faMusic} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pdf';
+
+  @ViewChild('content') content: ElementRef;
+  public downloadPDF(){
+    let doc = new jsPDF();
+    let specialElementHandlers={
+      '#editor':function(element,renderer){
+        return true;
+      }
+    };
+    let content = this.content.nativeElement;
+    doc.addHTML(content.innerHTML,0,0,{
+    'width':100,
+    'elementHandlers': specialElementHandlers
+    },
+    function (bla) { doc.save('saveInCallback.pdf'); },
+    0);
+  }
+  name = 'jsPDF Example';
+
+  faRoad = faRoad;
+  faMobile = faMobile;
+  faEnvelope = faEnvelope;
+  faBook = faBook;
+  faGamepad = faGamepad;
+  faMusic = faMusic;
+ 
+ 
+ 
+
+  constructor(private elementRef: ElementRef) {
+    //super()
+  }
+
+  htmlToPdf() {
+    let doc = new jsPDF('p', 'px', 'a4');
+  
+   doc.setTextColor(255, 0, 0)
+   doc.text(20, 40, 'This is red.')
+    let options = {
+    pagesplit: true
+    };
+    
+   doc.fromHTML(this.elementRef.nativeElement, 0, 0, options, ()=>{
+      doc.save('Test.pdf');
+   });
+    //doc.save('Test.pdf');
+
+}}*/
+
+import { Component } from '@angular/core';
+import * as html2pdf from 'html2pdf.js';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pdf-image';
+  downloadPDF(){
+    const option = {
+      filename:'cv.pdf',
+      image:{type:'jpeg'},
+      html2canvas:{},
+      jsPDF:{orientation:'portrait'}
+    };
+    const content: Element = document.getElementById('CVcontent');
+    html2pdf().from(content).set(option).save();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+  
+    BrowserModule,
+    FontAwesomeModule,
+    CommonModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
